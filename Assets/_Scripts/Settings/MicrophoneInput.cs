d7a316805c8f2196f6339c4cdb83604e378686c5
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2020-09-14 11:07:46 +0200

    Unity changed to 2020.1.2

---
+++ Assets/_Scripts/Settings/MicrophoneInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneInput : MonoBehaviour
+{
+    public List<string> deviceNames = new List<string>();
+    private AudioSource audioS;
+
+    void Start()
+    {
+        foreach (var device in Microphone.devices)
+        {
+            deviceNames.Add(device);
+        }
+
+        audioS = GetComponent<AudioSource>();
+        audioS.clip = Microphone.Start(deviceNames[0], true, 10, 44100);
+        audioS.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audioS.Play();
+    }
+    
+    void Update()
+    {
+        float[] spectrum = new float[64];
+
+        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Hanning);
+
+        for (int i = 1; i < spectrum.Length - 1; i++)
+        {
+            Debug.DrawLine(new Vector3(1+i, Mathf.Log(spectrum[i - 1]), 0), new Vector3(1+i, Mathf.Log(spectrum[i]), 3), Color.white);
+        }
+    }
+
+}
